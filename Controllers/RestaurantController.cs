eaca3f167da6add74d8810e05a6b66e761b965fc
Jeremy Hansen <jhansen1344@gmail.com>
2021-06-24 20:58:14 -0400

    Adds db context, restaurant models and controller

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,57 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantRaterAPI.Models;
+
+namespace RestaurantRaterAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RestaurantController : Controller
+    {
+        private RestaurantDbContext _context;
+        public RestaurantController(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> CreateRestaurant([FromForm] RestaurantEdit model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Restaurants.Add(new Restaurant(){
+                Name = model.Name,
+                Location = model.Location
+            });
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+            
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRestaurants()
+        {
+            var restaurants = await _context.Restaurants.ToListAsync();
+            return Ok(restaurants);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetRestaurantById(int id)
+        {
+            var restaurant = await _context.Restaurants.FindAsync(id);
+
+            if(restaurant is null)
+            {
+                return NotFound();
+            }
+            return Ok(restaurant);
+        }
+    }
+}
---
+++ Models/Restaurant.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantRaterAPI.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Location { get; set; }
+    }
+}
